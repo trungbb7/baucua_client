2537818683c9fc57d54cadb4f2109bfb3de07bf6
ToVanLuan <21130438@st.hcmuaf.edu.vn>
2024-10-26 01:22:25 +0700

    Xu li redux quan li trang thai

---
+++ src/redux/constants/GameBauCuaActions.js
@@ -0,0 +1,3 @@
+export const DAT_CUOC_BAU_CUA = 'DAT_CUOC_BAU_CUA';
+export const PLAY_GAME_BAU_CUA = 'PLAY_GAME_BAU_CUA';
+export const CHOI_LAI = 'CHOI_LAI';
---
+++ src/redux/rootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import GameBauCuaReducer from "./reducer/GameBauCuaReducer";
+
+export const rootReducer = combineReducers({
+    GameBauCuaReducer,
+})
