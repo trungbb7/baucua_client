b76519b63d3d51acc87306f08a2318f757115302
ToVanLuan <21130438@st.hcmuaf.edu.vn>
2024-11-06 01:10:43 +0700

    Thu thập số liệu hiệu suất

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,4 @@
+// Thu thập số liệu hiệu suất
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
